666e1bfa95d49a08f49a79cdff1dd2daad56852f
Niklas Mårdby <niklas.mardby@gmail.com>
2014-05-12 11:18:48 +0200

    first video done
    this commit is the code done for the first video. finished: show all
    subjects, add subject

---
+++ add_subject.php
@@ -0,0 +1,7 @@
+<form action="index.php" method="POST">
+	<p>
+		<label for="subject_name">Subject name: </label>
+		<input type="text" name="subject_name" />
+	</p>
+	<input type="submit" value="Add" />
+</form>
--- index.php
+++ index.php
@@ -1,5 +1,35 @@
+<ul>
+	<li><a href="add_subject.php">Add subject</a></li>
+
 <?php
 
-	// summary_project
+	$host = "localhost";
+	$dbname = "summary_project";
+	$username= "summary_project";
+	$password = "123456";
+	$dsn = "mysql:host=$host;dbname=$dbname";
+	$attr = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+	$pdo = new PDO($dsn, $username, $password, $attr);
+
+	// add new subject from form
+	if(!empty($_POST))
+	{
+		if($_POST['subject_name'] !== "")
+		{
+			$_POST = null;
+			$subject_name = filter_input(INPUT_POST, 'subject_name');
+			$statement = $pdo->prepare("INSERT INTO subjects (name) VALUES (:subject_name)");
+			$statement->bindParam(":subject_name", $subject_name);
+			if(!$statement->execute())
+				print_r($statement->errorInfo());
+		}
+	}
+
+	foreach($pdo->query("SELECT * FROM subjects ORDER BY name") as $row)
+	{	
+		echo "<li><a href=\"summaries.php?subject_id={$row['id']}\">{$row['name']}</a></li>";
+	}
 
 ?>
+
+</ul>
